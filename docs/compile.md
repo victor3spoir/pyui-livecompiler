074c5a61c671a9f39f9460d266a7d886b0d59a53
kvruntime <kvruntime@gmail.com>
2024-12-18 23:30:15 +0000

    feat():add icon & image to the app

---
+++ docs/compile.md
@@ -0,0 +1,12 @@
+# Compile the project
+
+```bash
+  pyinstaller -y -w --clean \
+    --name pyui-livecompiler \
+    --upx-exclude "Qt*.dll" \
+    --add-data "src/presentation/resources;presentation/resources" \
+    --icon=src/presentation/resources/images/logo.ico \
+    src/program.py
+```
+
+
--- src/presentation/app.py
+++ src/presentation/app.py
@@ -1,14 +1,27 @@
 # coding:utf-8
 import sys
 import typing
-from PyQt6.QtWidgets import QApplication
+from pathlib import Path
 
 from presentation.shell import Shell
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QApplication
 
 
 class Application(QApplication):
     def __init__(self) -> None:
         super().__init__(sys.argv)
+        self.setApplicationName("PYUI-LiveCompiler")
+        self.setApplicationDisplayName("PYUI-LiveCompiler")
+        self.setWindowIcon(
+            # FIXME: this must set correctly using resource manager/resource.qrc
+            QIcon(
+                Path(__file__)
+                .parent.joinpath("resources/images/logo.jpg")
+                .resolve()
+                .as_posix()
+            )
+        )
         self.shell = Shell()
         return
 
